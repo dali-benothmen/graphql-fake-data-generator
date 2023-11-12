04dda44d9fa51eaff756ec8998666a8ba95f3962
MedAli5543 <ben.othmen5543@gmail.com>
2023-11-13 00:10:05 +0100

    feat: Add tests for parseGraphQLSchema
    The commit adds tests for the `parseGraphQLSchema` function. It includes tests for reading the schema from a file, treating the input as a stringified schema when the file does not exist, throwing an error for an invalid schema, and logging and rethrowing errors on exceptions.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,53 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from 'vitest'
+import fs from 'fs'
+import path from 'path'
+import { GraphQLSchema } from 'graphql'
+import { parseGraphQLSchema } from './parse-graphql-schema'
 
-describe("whatever", () => {
-  it("should pass CI", () => {
-    expect(true).toBeTruthy();
-  });
-});
+vi.mock('fs')
+vi.mock('path')
+
+describe('parseGraphQLSchema', () => {
+  it('reads schema from file when file exists', () => {
+    const mockSchema = 'type Query { hello: String }'
+
+    vi.spyOn(fs, 'existsSync').mockReturnValue(true)
+    vi.spyOn(fs, 'readFileSync').mockReturnValue(mockSchema)
+    vi.spyOn(path, 'resolve').mockReturnValue('path/to/schema.graphql')
+
+    const schema = parseGraphQLSchema('schema.graphql')
+
+    expect(schema).toBeInstanceOf(GraphQLSchema)
+    expect(fs.readFileSync).toHaveBeenCalledWith('path/to/schema.graphql', 'utf-8')
+  })
+
+  it('treats input as stringified schema when file does not exist', () => {
+    const mockSchema = 'type Query { hello: String }'
+
+    vi.spyOn(fs, 'existsSync').mockReturnValue(false)
+
+    const schema = parseGraphQLSchema(mockSchema)
+
+    expect(schema).toBeInstanceOf(GraphQLSchema)
+  })
+
+  it('throws an error for invalid schema', () => {
+    const invalidSchema = 'invalid schema'
+
+    vi.spyOn(fs, 'existsSync').mockReturnValue(false)
+
+    expect(() => parseGraphQLSchema(invalidSchema)).toThrow()
+  })
+
+  it('logs and rethrows error on exception', () => {
+    const consoleSpy = vi.spyOn(console, 'error')
+    const error = new Error('Test Error')
+
+    vi.spyOn(fs, 'existsSync').mockImplementation(() => {
+      throw error
+    })
+
+    expect(() => parseGraphQLSchema('schema.graphql')).toThrow(error)
+    expect(consoleSpy).toHaveBeenCalledWith('Testing Error parsing schema:', error)
+  })
+})
