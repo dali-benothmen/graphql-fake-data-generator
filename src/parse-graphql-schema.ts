5763b396e8ad00c5c557ccbe0acf27a477acdabf
MedAli5543 <ben.othmen5543@gmail.com>
2023-11-12 23:14:38 +0100

    refactor(parse-graphql-schema): Simplify base path determination
    This commit simplifies the determination of the base path in the `parseGraphQLSchema` function. The fallback to `__dirname` is used when `require.main` is not available. Additionally, the code removes the comments that describe each step.

--- src/parse-graphql-schema.ts
+++ src/parse-graphql-schema.ts
@@ -6,19 +6,17 @@ export function parseGraphQLSchema(schemaInput: string): GraphQLSchema {
   try {
     let schemaString: string
 
-    // Determine the base path
     let basePath
+
     if (require.main && require.main.filename) {
       basePath = path.dirname(require.main.filename)
     } else {
-      basePath = __dirname // Fallback to __dirname if require.main is not available
+      basePath = __dirname
     }
 
-    // Attempt to construct the full path from the base path
     const filePath = path.resolve(basePath, schemaInput)
 
     if (fs.existsSync(filePath)) {
-      // Read the file if it exists
       schemaString = fs.readFileSync(filePath, 'utf-8')
     } else {
       // Treat as a stringified schema if the file doesn't exist
