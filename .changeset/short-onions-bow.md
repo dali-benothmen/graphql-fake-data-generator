399ec47b65b5843b859aeca20ded2e92bb025852
MedAli5543 <ben.othmen5543@gmail.com>
2023-11-12 22:40:41 +0100

    fix: Normalize file path for GraphQL schema loading
    This commit introduces changes to the function `parseGraphQLSchema` in the file `parse-graphql-schema.ts`.
    
    - Determining the base path has been enhanced to handle different scenarios.
    - The full path is resolved using the base path and the provided schema input.
    - If the file exists, it is read and assigned to `schemaString`.
    - If the file doesn't exist, the provided input is treated as a stringified schema.
    
    These changes ensure that the file path for loading the GraphQL schema is normalized and handled correctly, allowing for more flexibility when working with different file and input options.

---
+++ .changeset/short-onions-bow.md
@@ -0,0 +1,5 @@
+---
+"graphql-fake-data-generator": patch
+---
+
+PATCH: Normalize file path for GraphQL schema loading
--- src/parse-graphql-schema.ts
+++ src/parse-graphql-schema.ts
@@ -6,11 +6,22 @@ export function parseGraphQLSchema(schemaInput: string): GraphQLSchema {
   try {
     let schemaString: string
 
-    const filePath = path.join(__dirname, schemaInput)
+    // Determine the base path
+    let basePath
+    if (require.main && require.main.filename) {
+      basePath = path.dirname(require.main.filename)
+    } else {
+      basePath = __dirname // Fallback to __dirname if require.main is not available
+    }
+
+    // Attempt to construct the full path from the base path
+    const filePath = path.resolve(basePath, schemaInput)
 
     if (fs.existsSync(filePath)) {
+      // Read the file if it exists
       schemaString = fs.readFileSync(filePath, 'utf-8')
     } else {
+      // Treat as a stringified schema if the file doesn't exist
       schemaString = schemaInput
     }
 
