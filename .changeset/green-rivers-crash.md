4d97a93f6cbabdfcfac58591f8eb31271fc684f4
MedAli5543 <ben.othmen5543@gmail.com>
2023-11-12 21:23:20 +0100

    Refactor parseGraphQLSchema function to handle schema input from both file path and string.
    - Remove the isLikelyFilePath helper function as it is no longer needed.
    - Use a try-catch block to attempt reading the schema input as a file using fs.readFileSync.
    - If the file read fails, catch the error and attempt reading the schema input as a stringified schema.
    - If both reads fail, assume the input is a stringified schema.
    - Update the file read error variable name from fileError to dirnameReadError for clarity.

---
+++ .changeset/green-rivers-crash.md
@@ -0,0 +1,5 @@
+---
+"graphql-fake-data-generator": patch
+---
+
+Refactor parseGraphQLSchema function to handle schema input from both file path and string.
--- src/parse-graphql-schema.ts
+++ src/parse-graphql-schema.ts
@@ -2,24 +2,20 @@ import fs from 'fs'
 import path from 'path'
 import { buildSchema, GraphQLSchema } from 'graphql'
 
-function isLikelyFilePath(input: string): boolean {
-  return input.endsWith('.graphql') || input.endsWith('.gql')
-}
-
 export function parseGraphQLSchema(schemaInput: string): GraphQLSchema {
   try {
     let schemaString: string
 
-    if (isLikelyFilePath(schemaInput)) {
+    try {
+      schemaString = fs.readFileSync(schemaInput, 'utf-8')
+    } catch (directReadError) {
       try {
-        const resolvedPath = path.resolve(schemaInput)
+        const resolvedPath = path.resolve(__dirname, schemaInput)
         schemaString = fs.readFileSync(resolvedPath, 'utf-8')
-      } catch (fileError) {
-        console.error('Error reading schema file:', fileError)
-        throw fileError
+      } catch (dirnameReadError) {
+        // If both reads fail, assume it's a stringified schema
+        schemaString = schemaInput
       }
-    } else {
-      schemaString = schemaInput
     }
 
     return buildSchema(schemaString)
