0de882a41b906fc1f676086a94c80aa90727bc01
MedAli5543 <ben.othmen5543@gmail.com>
2023-11-13 00:51:31 +0100

    refactor GraphQL imports and update paths in index.test.ts
    - Refactor GraphQL imports in index.test.ts to import only necessary types.
    - Update paths for AnalyzedSchema, AnalyzedObjectType, and AnalyzedEnumType imports in index.test.ts.
    
    Add new tests and functions for schema analysis and generating mock data
    
    - Add tests for analyzing schemas with object types, enum types, and field types including lists and non-nulls.
    - Add functions isAnalyzedObjectType and isAnalyzedEnumType to check if a given type is an AnalyzedObjectType or AnalyzedEnumType.
    - Add analyzeSchema function to analyze a given GraphQLSchema and return an analyzed schema object.
    - Add generateMockData function to generate mock data based on an analyzed schema object.
    
    Implement generateMockData function to generate mock data
    
    - Implement generateMockData function to generate mock data using a given analyzed schema object.
    - Handle basic types, list fields, and custom generators.

---
+++ .changeset/loud-donkeys-share.md
@@ -0,0 +1,5 @@
+---
+"graphql-fake-data-generator": patch
+---
+
+Add new tests and functions for schema analysis and generating mock data
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,30 @@
 import { describe, it, expect, vi } from 'vitest'
 import fs from 'fs'
 import path from 'path'
-import { GraphQLSchema } from 'graphql'
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLEnumType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLList,
+} from 'graphql'
 import { parseGraphQLSchema } from './parse-graphql-schema'
+import { analyzeSchema } from './analyze-schema'
+import { generateMockData } from './generate-mock-data'
+import { AnalyzedSchema, AnalyzedObjectType, AnalyzedEnumType } from './types' // Update with the actual path
 
 vi.mock('fs')
 vi.mock('path')
+
+function isAnalyzedObjectType(type: any): type is AnalyzedObjectType {
+  return type.kind === 'OBJECT'
+}
+
+function isAnalyzedEnumType(type: any): type is AnalyzedEnumType {
+  return type.kind === 'ENUM'
+}
 
 describe('parseGraphQLSchema', () => {
   it('reads schema from file when file exists', () => {
@@ -38,16 +57,138 @@ describe('parseGraphQLSchema', () => {
 
     expect(() => parseGraphQLSchema(invalidSchema)).toThrow()
   })
+})
 
-  it('logs and rethrows error on exception', () => {
-    const consoleSpy = vi.spyOn(console, 'error')
-    const error = new Error('Test Error')
-
-    vi.spyOn(fs, 'existsSync').mockImplementation(() => {
-      throw error
+describe('analyzeSchema', () => {
+  it('analyzes schema with object types', () => {
+    const schema = new GraphQLSchema({
+      query: new GraphQLObjectType({
+        name: 'Query',
+        fields: {
+          hello: { type: GraphQLString },
+        },
+      }),
     })
 
-    expect(() => parseGraphQLSchema('schema.graphql')).toThrow(error)
-    expect(consoleSpy).toHaveBeenCalledWith('Testing Error parsing schema:', error)
+    const analyzed = analyzeSchema(schema)
+    const queryType = analyzed.Query
+
+    if (isAnalyzedObjectType(queryType)) {
+      expect(queryType.kind).toBe('OBJECT')
+      expect(queryType.fields.hello.type).toBe('String')
+    } else {
+      throw new Error('Expected Query to be an AnalyzedObjectType')
+    }
+  })
+
+  it('analyzes schema with enum types', () => {
+    const myEnum = new GraphQLEnumType({
+      name: 'MyEnum',
+      values: {
+        ONE: { value: 1 },
+        TWO: { value: 2 },
+      },
+    })
+
+    const schema = new GraphQLSchema({
+      query: new GraphQLObjectType({
+        name: 'Query',
+        fields: {
+          myEnum: { type: myEnum },
+        },
+      }),
+    })
+
+    const analyzed = analyzeSchema(schema)
+    const myEnumType = analyzed.MyEnum
+
+    if (isAnalyzedEnumType(myEnumType)) {
+      expect(myEnumType.values).toEqual(['ONE', 'TWO'])
+    } else {
+      throw new Error('Expected MyEnum to be an AnalyzedEnumType')
+    }
+  })
+
+  it('analyzes field types including lists and non-nulls', () => {
+    const schema = new GraphQLSchema({
+      query: new GraphQLObjectType({
+        name: 'Query',
+        fields: {
+          numbers: { type: new GraphQLNonNull(new GraphQLList(GraphQLInt)) },
+        },
+      }),
+    })
+
+    const analyzed = analyzeSchema(schema)
+    const queryType = analyzed.Query
+
+    if (isAnalyzedObjectType(queryType)) {
+      const numbersField = queryType.fields.numbers
+
+      expect(numbersField.type).toBe('Int')
+      expect(numbersField.isList).toBe(true)
+      expect(numbersField.isNonNull).toBe(true)
+    } else {
+      throw new Error('Expected Query to be an AnalyzedObjectType')
+    }
+  })
+})
+
+describe('generateMockData', () => {
+  it('generates mock data for basic types', () => {
+    const schema: AnalyzedSchema = {
+      Query: {
+        kind: 'OBJECT',
+        fields: {
+          id: { type: 'ID', isList: false, isNonNull: true },
+          name: { type: 'String', isList: false, isNonNull: false },
+        },
+      },
+    }
+
+    const mockData = generateMockData(schema)
+    expect(mockData.id).toBeDefined()
+    expect(typeof mockData.id).toBe('string')
+    expect(mockData.name).toBeDefined()
+    expect(typeof mockData.name).toBe('string')
+  })
+
+  it('handles list fields', () => {
+    const schema: AnalyzedSchema = {
+      Query: {
+        kind: 'OBJECT',
+        fields: {
+          numbers: { type: 'Int', isList: true, isNonNull: false },
+        },
+      },
+    }
+
+    const mockData = generateMockData(schema, { listLength: 3 })
+    expect(Array.isArray(mockData.numbers)).toBe(true)
+    expect(mockData.numbers).toHaveLength(3)
+  })
+
+  it('uses custom generators', () => {
+    const customNameGenerator = () => 'customName'
+    const schema: AnalyzedSchema = {
+      Query: {
+        kind: 'OBJECT',
+        fields: {
+          name: { type: 'String', isList: false, isNonNull: false },
+        },
+      },
+    }
+
+    const options = {
+      query: {
+        name: {
+          name: customNameGenerator,
+        },
+      },
+    }
+
+    const mockData = generateMockData(schema, options)
+
+    expect(mockData.name).toBe('customName')
   })
 })
